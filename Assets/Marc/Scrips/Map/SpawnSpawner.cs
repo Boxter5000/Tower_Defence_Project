315be69c83640547991d0dfee2597ffc5932bfdb
Boxter5000 <marc.schuster60@web.de>
2021-04-07 23:54:25 +0200

    Revert "added estate path building"
    This reverts commit 94319095cbddc66614d392c19d16df3220929eea.

--- Assets/Marc/Scrips/Map/SpawnSpawner.cs
+++ Assets/Marc/Scrips/Map/SpawnSpawner.cs
@@ -12,30 +12,15 @@ public class SpawnSpawner : MonoBehaviour {
     [SerializeField] private Vector2[] spawnPositions = new Vector2[4];
 
     [SerializeField] private GameObject spawnPrefab;
-    [SerializeField] private GameObject estatePrefab;
 
     private int OpeningDir;
 
     private void Awake() {
-        Debug.Log(transform.position);
         for(int i = 0; i < spawnPositions.Length; i++) {
-            Vector2 checkPosition = spawnPositions[i] + (Vector2)transform.position;
-            Collider2D coll = Physics2D.OverlapBox(checkPosition, Vector2.one, 0.0f);
-            if(coll == null) {
-                if(checkPosition.x != 0.5d && checkPosition.y != 0.5d) {
-                    Instantiate(estatePrefab, (Vector2)transform.position + spawnPositions[i], Quaternion.identity, transform);
-                }
-                else {
-                    GameObject NewSpawner = Instantiate(spawnPrefab, (Vector2)transform.position + spawnPositions[i], Quaternion.identity, transform);
-                    NewSpawner.GetComponent<PathSpawner>().OpeningDirection = i;
-                }
-            }
-            else {
-                if (coll.gameObject.CompareTag("FreeRealEstate")) {
-                    Destroy(coll.gameObject);
-                    GameObject NewSpawner = Instantiate(spawnPrefab, (Vector2)transform.position + spawnPositions[i], Quaternion.identity, transform);
-                    NewSpawner.GetComponent<PathSpawner>().OpeningDirection = i;
-                }
+            Collider2D coll = Physics2D.OverlapBox(spawnPositions[i], Vector2.one, 0.0f);
+            if(coll != null) {
+                GameObject NewSpawner = Instantiate(spawnPrefab, (Vector2)transform.position + spawnPositions[i], Quaternion.identity, transform);
+                NewSpawner.GetComponent<PathSpawner>().OpeningDirection = i;
             }
         }
     }
