e755953cd5c2a538f0bdd6e86adcc273af7b360a
Boxter5000 <marc.schuster60@web.de>
2021-04-08 00:02:09 +0200

    Finaly Fiexes worked

--- Assets/Marc/Scrips/Map/SpawnSpawner.cs
+++ Assets/Marc/Scrips/Map/SpawnSpawner.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnSpawner : MonoBehaviour {
+public class SpawnSpawner : MonoBehaviour
+{
     /// <summary>
-    /// index 0 = top1
+    /// index 0 = top
     /// index 1 = bottom
     /// index 2 = left
     /// index 3 = right
@@ -12,15 +13,37 @@ public class SpawnSpawner : MonoBehaviour {
     [SerializeField] private Vector2[] spawnPositions = new Vector2[4];
 
     [SerializeField] private GameObject spawnPrefab;
+    [SerializeField] private GameObject estatePrefab;
 
     private int OpeningDir;
 
-    private void Awake() {
-        for(int i = 0; i < spawnPositions.Length; i++) {
-            Collider2D coll = Physics2D.OverlapBox(spawnPositions[i], Vector2.one, 0.0f);
-            if(coll != null) {
-                GameObject NewSpawner = Instantiate(spawnPrefab, (Vector2)transform.position + spawnPositions[i], Quaternion.identity, transform);
-                NewSpawner.GetComponent<PathSpawner>().OpeningDirection = i;
+    private void Awake()
+    {
+        Debug.Log(transform.position);
+        for (int i = 0; i < spawnPositions.Length; i++)
+        {
+            Vector2 checkPosition = spawnPositions[i] + (Vector2)transform.position;
+            Collider2D coll = Physics2D.OverlapBox(checkPosition, Vector2.one, 0.0f);
+            if (coll == null)
+            {
+                if (checkPosition.x != 0.5d && checkPosition.y != 0.5d)
+                {
+                    Instantiate(estatePrefab, (Vector2)transform.position + spawnPositions[i], Quaternion.identity, transform);
+                }
+                else
+                {
+                    GameObject NewSpawner = Instantiate(spawnPrefab, (Vector2)transform.position + spawnPositions[i], Quaternion.identity, transform);
+                    NewSpawner.GetComponent<PathSpawner>().OpeningDirection = i;
+                }
+            }
+            else
+            {
+                if (coll.gameObject.CompareTag("FreeRealEstate"))
+                {
+                    Destroy(coll.gameObject);
+                    GameObject NewSpawner = Instantiate(spawnPrefab, (Vector2)transform.position + spawnPositions[i], Quaternion.identity, transform);
+                    NewSpawner.GetComponent<PathSpawner>().OpeningDirection = i;
+                }
             }
         }
     }
