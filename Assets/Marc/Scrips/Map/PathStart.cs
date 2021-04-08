7c9e4bf5db5cc2dd917ebf8689f35c93135dce9d
Dethyser <71837325+Dethyser@users.noreply.github.com>
2021-04-08 12:58:29 +0200

    started adding subelement fields

--- Assets/Marc/Scrips/Map/PathStart.cs
+++ Assets/Marc/Scrips/Map/PathStart.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class PathStart : MonoBehaviour
 {
-    Entity entity;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         GameObject EnemyCollidet = collision.gameObject;
 
         if(EnemyCollidet != null)
         {
-            entity = EnemyCollidet.GetComponent<Entity>();
-            entity.SetPath(transform.parent.GetComponent<Path>());
+            Entity entity = EnemyCollidet.GetComponent<Entity>();
+            entity?.SetPath(transform.parent.GetComponent<Path>());
         }
     }
 }
