a70072ee58a32d5a40cbe2b6227f38404b25c1a4
Boxter5000 <marc.schuster60@web.de>
2021-04-23 16:40:44 +0200

    Started organising the first tower script

--- Assets/Marc/Scrips/Towers/TowerBase.cs
+++ Assets/Marc/Scrips/Towers/TowerBase.cs
@@ -4,72 +4,90 @@ using UnityEngine;
 
 public class TowerBase : MonoBehaviour
 {
-    public string Tag;
-    public float offset;
+    [Header("References")]
+        [SerializeField] private CircleCollider2D RangeCollider;
+        [SerializeField] private Bullet1 Ammo;
 
-    public Bullet1 Bullet;
-    public float BulletVelocity;
-    public float shootingRate;
-    public float Range;
-    public float TowerDamage;
-    [SerializeField] CircleCollider2D RangeCollider;
+        [HideInInspector] private Transform target;                 //Reference to the target to shoot
+        [HideInInspector] private Vector3 targetPos;                //Position of that Target
+        [HideInInspector] private Vector3 thisPos;                  //Position of this Tower
+        [HideInInspector] private bool InRange = true;
 
-    private Transform target;
-    private Vector3 targetPos;
-    private Vector3 thisPos;
-    private float angle;
-    private bool CR_runing;
-    private bool IsShootingEnabled = true;
+    [Header("Tower Specs")]
+        [SerializeField] private float BulletVelocity;
+        [SerializeField] private float shootingRate;
+        [SerializeField] private float Damage;
+        [SerializeField] private float Range;
+        [SerializeField] private float RotationalOffset;
 
-    List<GameObject> EnemysList;
-
-    private bool InRange = true;
+    //Other Information
+        [HideInInspector] private float angle;
+        [HideInInspector] private bool CR_runing;                   //Is the Coroutine currently running?
+        [HideInInspector] private bool IsShootingEnabled = true;    //Is the Tower allowed to shoot (for draging and placing Towers)
+        [HideInInspector] List<GameObject> EnemysList;
 
     private void Awake()
     {
         EnemysList = new List<GameObject>();
         RangeCollider.radius = Range;
+        thisPos = transform.position;
     }
+
+    IEnumerator shootTimer()
+    {
+
+
+
+        if (IsShootingEnabled)
+        {
+
+
+
+            CR_runing = true;
+
+
+            //while something is in range, shoot
+
+
+            while(EnemysList.Count != 0)
+            {
+
+
+                GetTargetPos();
+
+                InstantiateShoot();
+
+                yield return new WaitForSeconds(shootingRate);
+            }
+
+
+            CR_runing = false;
+        }
+    }
+
 
     public void GetTargetPos()
     {
         if (IsShootingEnabled)
         {
             targetPos = EnemysList[0].transform.position;
-            thisPos = transform.position;
+            targetPos -= thisPos;
 
-            targetPos = targetPos - thisPos;
             //calkulate the radius and the angel of the rotation
             angle = Mathf.Atan2(targetPos.y, targetPos.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle + offset));
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle + RotationalOffset));
+
             if (target != null)
-            {
                 Debug.DrawLine(thisPos, target.position);
-            }
         }
     }
     
-    IEnumerator shootTimer()
-    {
-        if (IsShootingEnabled)
-        {
-            CR_runing = true;
-            //while something is in range, shoot
-            while(EnemysList.Count != 0)
-            {
-                GetTargetPos();
-                InstantiateShoot();
-                yield return new WaitForSeconds(shootingRate);
-            }
-            CR_runing = false;
-        }
-    }
 
     public void InstantiateShoot()
     {
         Vector3 VelocityDirection = new Vector3(targetPos.x, targetPos.y, 0).normalized;
-        Bullet1 BulletClone = Instantiate(Bullet, transform.position, Quaternion.identity);
-        BulletClone.Damage = TowerDamage;
+        Bullet1 BulletClone = Instantiate(Ammo, transform.position, Quaternion.identity);
+        BulletClone.Damage = Damage;
         BulletClone.GiveDirection(VelocityDirection, BulletVelocity);
     }
 
